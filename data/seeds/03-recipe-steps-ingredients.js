6d3b7d1b65a02d1f7dd27d7a11662553fd7690f3
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-22 03:10:43 -0500

    all seeds successfully run

--- data/seeds/03-recipe-steps-ingredients.js
+++ data/seeds/03-recipe-steps-ingredients.js
@@ -5,8 +5,16 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("recipe_steps_ingredients").insert([
-        { ingredient_id: 1, recipe_step_id: 1, quantity: "A whole gallon" },
-        { ingredient_id: 2, recipe_step_id: 2, quantity: "Two whole gallons" },
+        {
+          ingredient_id: 1,
+          recipe_step_id: 1,
+          quantity: "A whole gallon",
+        },
+        {
+          ingredient_id: 2,
+          recipe_step_id: 2,
+          quantity: "Two whole gallons",
+        },
         {
           ingredient_id: 3,
           recipe_step_id: 3,
